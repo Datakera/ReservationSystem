32a902ed942a44a0c7e3421c3c92c1ef4666598a
Datakera <juanpablosilva.vasquez2610@gmail.com>
2023-11-13 17:53:56 -0500

    junit colocado

--- src/test/java/service/ReservationSystemTest.java
+++ src/test/java/service/ReservationSystemTest.java
@@ -1,8 +1,10 @@
 package service;
 
+import dao.ConferenceRoomDao;
 import model.ConferenceRoom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,17 +14,44 @@ public class ReservationSystemTest {
 
     @BeforeEach
     public void setUp() {
-        //system = new ReservationSystem();
+        MockitoAnnotations.openMocks(this);
+
     }
 
     @Test
     public void testReserveRoom() {
         // Implementar pruebas aquí
+
+        ConferenceRoom room1 = new ConferenceRoom("1", 10);
+        ConferenceRoom room2 = new ConferenceRoom("2", 20);
+        ConferenceRoom room3 = new ConferenceRoom("3", 35);
+        ConferenceRoom room4 = new ConferenceRoom("4", 49);
+        room2.setReserved(true);
+
+        assertEquals ("1", room1.getId());
+        assertEquals(10, room1.getCapacity());
+        assertFalse(room1.isReserved());
+
+        assertEquals ("2", room2.getId());
+        assertEquals(20, room2.getCapacity());
+        assertTrue(room2.isReserved());
+
+
     }
 
     @Test
     public void testCancelReservation() {
         // Implementar pruebas aquí
+
+        ConferenceRoom room2 = new ConferenceRoom("2", 12);
+        room2.setReserved(true);
+        
+
+        boolean cancellationService = system.cancelReservation("2");
+
+        assertTrue(cancellationService);
+        assertFalse(room2.isReserved());
+
     }
 
     // Puedes añadir más pruebas según lo consideres necesario
